febc1ba5d0d9bfa2a7bab54a9b733d1e333c174f
rockts <rockts@sina.com>
2020-07-27 12:24:56 +0800

    路由：Router

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -3,10 +3,10 @@ import { Request, Response, NextFunction } from 'express';
 /**
  * 内容列表
  */
-export const index = {
+export const index = (
   request: Request,
   response: Response,
-  next: NextFunction
-} => {
-  response.send('内容列表接口')
+  next: NextFunction,
+) => {
+  response.send('内容列表接口');
 };
