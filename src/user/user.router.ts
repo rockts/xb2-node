4508d69f9e6b2af0be4bb0cf82a64360dbf72091
rockts <rockts@sina.com>
2020-07-28 03:25:21 +0800

    Hash 用户密码<Bcrypt>

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import * as userController from './user.controller';
-import { validataUserData } from './user.middleware';
+import { validataUserData, hashPassword } from './user.middleware';
 
 const router = express.Router();
 
 /**
  * 创建用户
  */
-router.post('/users', validataUserData, userController.store);
+router.post('/users', validataUserData, hashPassword, userController.store);
 
 /**
  * 导出路由
