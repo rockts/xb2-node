24b6c00f12481f5d89f6373820c23efca1b3910e
rockts <rockts@sina.com>
2020-07-28 02:54:56 +0800

    验证用户数据

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,2 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from './user.service';
+import { request } from 'http';
+
+/**
+ * 验证用户数据
+ */
+export const validataUserData = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  console.log('👮‍♂️ 验证用户数据');
+
+  // 准备数据
+  const { name, password } = request.body;
+
+  // 验证必填数据
+  if (!name) return next(new Error('NAME_IS_REQUIRED'));
+  if (!password) return next(new Error('PASSWORD_REQUIRED'));
+
+  // 下一步
+  next();
+};
