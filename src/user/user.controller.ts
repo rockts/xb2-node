1a406f8e6d3e15d0164a031971cb9b4dfd1f5ea6
rockts <rockts@sina.com>
2020-07-28 02:42:29 +0800

    定义创建用户接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,3 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 import { UserModel } from './user.model';
 import * as userService from './user.service';
+import { request } from 'http';
+
+/**
+ * 创建用户
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { name, password } = request.body;
+
+  // 创建用户
+  try {
+    const data = await userService.createUser({ name, password });
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
