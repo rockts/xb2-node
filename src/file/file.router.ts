0a54512b5fa5df8df2964b195b6387f0bb216910
rockts <rockts@sina.com>
2020-07-31 22:13:41 +0800

    定义上传文件接口

--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import * as fileController from './file.controller';
+import { authGuard } from '../auth/auth.middleware';
+import { fileInterceptor } from './file.middleware';
 
 const router = express.Router();
 
 /**
+ * 上传文件
+ */
+router.post('/files', authGuard, fileInterceptor, fileController.store);
+
+/**
  * 导出路由
  */
-
 export default router;
