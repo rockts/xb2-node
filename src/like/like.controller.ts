64946fad26a707e02f250c4057cb94a4ead1d7cc
rockts <rockts@sina.com>
2020-08-29 00:28:26 +0800

    定义取消点赞内容接口

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { createUserLikePost } from './like.service';
+import { createUserLikePost, deleteUserLikePost } from './like.service';
 
 /**
  * 点赞内容
@@ -21,5 +21,28 @@ export const storeUserLikePost = async (
     response.status(201).send(data);
   } catch (error) {
     next(error)
+  }
+};
+
+/**
+ * 取消点赞内容
+ */
+export const destroyUserLikePost = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { postId } = request.params;
+  const { id: userId } = request.user;
+
+  // 取消点赞内容
+  try {
+    const data = await deleteUserLikePost(userId, parseInt(postId, 10));
+
+    // 做出响应
+    response.send(data);
+  } catch (error) {
+    next(error);
   }
 };
